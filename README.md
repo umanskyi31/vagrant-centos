b284f9e14d3cb23102c7605c828727bd8b86ef00
Vlad Umanskyi <vladumanskyi@gmail.com>
2018-06-11 22:53:44 +0300

    Prepera to production

--- README.md
+++ README.md
@@ -1,9 +1,26 @@
-Configuration Vagrant 
-=====================
+Vagrant development environment  
+=======
+It's easy configure and portable work development environment.
 
-![vagrant](Vagrant.png)
+This project is pre-build package with CentOS 7.1, Apache 2.4, PHP 7.1, MariaDB 5.5 and other software like as composer and git.
+
+## Getting Started
+
+**Requirement**
+
+* Install [VirtualBox](https://www.virtualbox.org/wiki/Downloads)
+* Install [Vagrant](http://www.vagrantup.com/)
+* Clone or [download](https://github.com/umanskyi31/vagrant-centos) this repository to the root folder `git clone https://github.com/umanskyi31/vagrant-centos`
+* Run `vagrant up` into root folder
+
+The first time you run this, Vagrant will download the CentOS box image. Will run shell script which install next software (Apache 2.4, PHP 7.1, MariaDB 5.5 and etc.)
+
+## How to use
+
+* In your browser, head to `192.168.33.33`. If need to change the address, you can do this into Vagrantfile:
+ 
+ ```  config.vm.network "private_network", ip: "192.168.33.33"```
+* Database name is vagrant_db, user = vagrant / password = secret
 
 
-* LEMP on CentOS-7
-* Using Bash
-* Using ansible [Ansible documentation](http://docs.ansible.com/)
+**ENJOY**
--- bootstrap.sh
+++ bootstrap.sh
@@ -23,27 +23,26 @@ sudo yum -y install mariadb-server mariadb
 sudo systemctl start mariadb.service
 sudo systemctl enable mariadb.service
 
-mysql -u root -e "CREATE DATABASE IF NOT EXISTS project"
-mysql -u root -e "GRANT ALL PRIVILEGES ON project.* TO 'project'@'localhost' IDENTIFIED BY 'password'"
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS vagrant_db"
+mysql -u root -e "GRANT ALL PRIVILEGES ON vagrant_db.* TO 'vagrant'@'localhost' IDENTIFIED BY 'secret'"
 mysql -u root -e "FLUSH PRIVILEGES"
 sudo yum -y install unzip
 #----END MARIADB-----
 
 echo "INCLUDE PHP"
 #Install and enable EPEL and Remi repository to your CentOS 7 system
-yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-yum install -y http://rpms.remirepo.net/enterprise/remi-release-7.rpm
+sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+sudo yum install -y http://rpms.remirepo.net/enterprise/remi-release-7.rpm
 
 #You need to install yum-utils, a collection of useful programs for managing yum repositories and packages.
 # It has tools that basically extend yum’s default features.
-yum install -y yum-utils
+sudo yum install -y yum-utils
 
 #One of the programs provided by yum-utils is yum-config-manager,
 # which you can use to enable Remi repository as the default repository for installing different PHP versions as shown
-yum-config-manager --enable remi-php71
-
+sudo yum-config-manager --enable remi-php71
 #install php
-yum install -y php71 php-mcrypt php-cli php-gd php-curl php-mysql php-ldap php-zip php-fileinfo  php-mbstring php-xml
+sudo yum install -y php71 php-mcrypt php-cli php-gd php-curl php-mysql php-ldap php-zip php-fileinfo  php-mbstring php-xml
 #---END PHP----
 
 echo "INCLUDE ADDITIONAL SOFT"
@@ -66,20 +65,17 @@ sudo yum install -y vim
 
 echo "START CONFIGURED PROJECT"
 
-echo "CREATE FOLDER"
-sudo mkdir /vagrant/public_html/
-
 echo "CREATE SYMBOLIC LINK"
-sudo ln -s /vagrant/public_html/ /var/www/html/
+sudo ln -s /vagrant/ /var/www/html/
 
 echo "CREATE VIRTUAL HOST"
 # Create virtual host for Apache
 VHOST=$(cat <<EOF
 <VirtualHost *:80>
   DirectoryIndex index.php
-  DocumentRoot "/var/www/html/public_html"
+  DocumentRoot "/var/www/html/vagrant/"
   ServerName localhost
-  <Directory "/var/www/html/public_html">
+  <Directory "/var/www/html/vagrant/">
     AllowOverride All
   </Directory>
 </VirtualHost>
@@ -88,7 +84,7 @@ EOF
 
 sudo echo "$VHOST" >> /etc/httpd/conf/httpd.conf
 
-echo "RESTART APACHE \n END OF CONFIGURATION"
+echo "RESTART APACHE END OF CONFIGURATION"
 # Restart apache
 sudo systemctl restart httpd.service
 
